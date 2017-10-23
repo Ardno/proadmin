425289eef3b80080aa9b37a2c875eff4d4d1d9ff
Ardno <2372456694@qq.com>
2017-10-23 22:53:24 +0800

    添加事件类型

--- src/views/gridbusiness/index.vue
+++ src/views/gridbusiness/index.vue
@@ -4,14 +4,14 @@
       <el-tab-pane label="事件类型" name="first">
         <!-- 头部 -->
         <div class="layui-elem-quote">
-          <el-select clearable class="filter-item" style="width: 130px" v-model="listQuery.dept_id" placeholder="请选择部门">
+          <el-select clearable class="filter-item" style="width: 130px" @change="setDepid(id)" v-model="listQuery.dept_id" placeholder="请选择部门">
             <el-option v-for="item in  depArr" :key="item._id" :label="item.name" :value="item._id">
             </el-option>
           </el-select>
-          <el-button class="filter-item" type="primary" icon="plus">添加类型</el-button>
+          <el-button class="filter-item" @click="dialogFormVisible=true;this.titles='添加事件类型'" type="primary" icon="plus">添加类型</el-button>
         </div>
         <!-- table -->
-                <el-table :key='tableKey' :data="eventArr" v-loading="listLoading" element-loading-text="给我一点时间" border fit highlight-current-row style="width: 100%">
+        <el-table :key='tableKey' :data="eventArr" v-loading="listLoading" element-loading-text="给我一点时间" border fit highlight-current-row style="width: 100%">
           <el-table-column align="center" label="序号" width="65">
             <template scope="scope">
               <span>{{scope.row._id}}</span>
@@ -34,7 +34,7 @@
             </el-table-column>
           <el-table-column align="center" label="操作" width="190">
             <template scope="scope">
-              <el-button size="small" type="success">修改
+              <el-button size="small" type="success" @click="updateshow(scope.row)">修改
               </el-button>
             </template>
           </el-table-column>
@@ -42,16 +42,45 @@
       </el-tab-pane>
       <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
     </el-tabs>
+    <!-- 添加修改事件类型 -->
+    <el-dialog :title="titles" :visible.sync="dialogFormVisible" size="tiny">
+      <el-form class="small-space" :model="eventInfo" :rules="infoRules" ref="infoForm" label-position="right" label-width="120px">
+        <el-form-item label="部门">
+          <el-select clearable v-model="eventInfo.dept_id" placeholder="请选择部门">
+            <el-option v-for="item in  depArr" :key="item._id" :label="item.name" :value="item._id">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="事件类型名称" prop="name">
+          <el-input v-model="eventInfo.name" maxlength="30"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleUpdateCreate">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import store from '@/store'
 import { fetchDepartments } from '@/api/department'
-import { getEventTypeArr } from '@/api/depevent'
+import { getEventTypeArr, addEventType, updateEventType } from '@/api/depevent'
 
 export default {
   data() {
     return {
+      dialogFormVisible: false,
+      eventInfo: {
+        _id: '',
+        dept_id: '',
+        name: ''
+      },
+      titles: '添加事件类型',
+      infoRules: {
+        name: [{ required: true, trigger: 'blur', message: '请输入名称' }]
+      },
       activeName: 'first',
       depArr: [],
       eventArr: [],
@@ -63,15 +92,67 @@ export default {
     }
   },
   created() {
+    this.listQuery.dept_id = store.getters.useinfo
     this.getEventTypeArr()
     this.getDepArr()
   },
   methods: {
-    handleClick(tab, event) { // tab切换
+    setDepid(id) {
+      this.dept_id = id
+      console.log(id)
+    },
+    updateshow(item) {
+      this.titles = '修改事件类型'
+      this.dialogFormVisible = true
+      this.eventInfo._id = item._id
+    },
+    handleUpdateCreate() {
+      this.$refs.infoForm.validate(valid => {
+        if (valid) {
+          if (this.titles === '添加事件类型') {
+            this.eventInfo._id = null
+            addEventType(this.eventInfo).then(response => {
+              this.getEventTypeArr()
+              this.$message({
+                message: '添加成功',
+                type: 'success',
+                duration: 4 * 1000
+              })
+            }).catch(() => {
+              this.$message({
+                message: '添加失败，请稍后再试',
+                type: 'error',
+                duration: 4 * 1000
+              })
+            })
+          } else {
+            updateEventType(this.eventInfo).then(response => {
+              this.getEventTypeArr()
+              this.$message({
+                message: '修改成功',
+                type: 'success',
+                duration: 4 * 1000
+              })
+            }).catch(() => {
+              this.$message({
+                message: '修改失败，请稍后再试',
+                type: 'error',
+                duration: 4 * 1000
+              })
+            })
+          }
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    handleClick(tab, event) {
+      // tab切换
       console.log(tab, event)
     },
     getEventTypeArr() {
-      getEventTypeArr().then(res => {
+      getEventTypeArr(this.listQuery).then(res => {
         this.eventArr = res.info
       })
     },
