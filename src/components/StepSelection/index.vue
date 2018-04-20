58aa52976c481bc76622a972c2ccf62996577da2
e <luocheng@romoss.com>
2018-04-20 10:41:16 +0800

    组件

---
+++ src/components/StepSelection/index.vue
@@ -0,0 +1,20 @@
+<!--  -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'StepSelection',
+  data() {
+    return {
+    }
+  },
+  components: {},
+  computed: {},
+  created() {},
+  methods: {}
+}
+</script>
+<style lang='scss' scoped>
+</style>
